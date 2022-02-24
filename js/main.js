ee2a6f193db2ce54864401934ffe208d4fbb0322
SimoneScapin <simonescap87@gmail.com>
2022-02-24 15:48:34 +0100

    aggiunto testo al contenitore

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 const wrapper = document.getElementById('my-wrapper')
 
+
 for  (let i = 0 ; i< 100 ; i++){
 
     let newDiv = document.createElement('div');
-    newDiv.classList.add('box', 'm-4');
-    newDiv.innerHTML = i;
+    newDiv.classList.add('box', 'm-4', 'text-center');
+    newDiv.innerText = i;
     wrapper.append(newDiv);
 
-    if (i % 15 == 0) ; 
-else if (i % 3 == 0) ;
-else if (i % 5 == 0) ;
+    if (i % 15 == 0)   {
+    newDiv.innerHTML = "FizzBuzz";
+    }
+    
+else if (i % 3 == 0) {
+    newDiv.innerHTML = "Fizz";
+}
+
+else if (i % 5 == 0) {
+    newDiv.innerHTML = "Buzz";
+}
 else;
 }
